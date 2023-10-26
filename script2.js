3ba1421bacf728d4cb915b1d06904206870763d1
Karthik Ramesan <136253498+fsydurden@users.noreply.github.com>
2023-10-26 06:39:36 +0530

    add white mode

---
+++ script2.js
@@ -0,0 +1,7 @@
+var image = document.getElementById("test");
+function changebg(){
+    document.body.style.backgroundColor = "white";
+}
+
+//event listner
+image.addEventListener("click",changebg);
